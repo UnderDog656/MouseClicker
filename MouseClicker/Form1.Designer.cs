096cecdd2827551e62db0c54011c15cb12671537
hungrytv13 <hungrytv13@hotmail.com>
2019-03-20 18:10:14 +0000

    Add project files.

---
+++ MouseClicker/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace MouseClicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.7F, System.Drawing.FontStyle.Bold);
+            this.textBox1.Location = new System.Drawing.Point(290, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(171, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(297, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Find Mouse Position";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.7F, System.Drawing.FontStyle.Bold);
+            this.textBox3.Location = new System.Drawing.Point(290, 137);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(171, 21);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(287, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 15);
+            this.label2.TabIndex = 8;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.button4.Location = new System.Drawing.Point(10, 10);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 40);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Start Auto Clicker";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label6.Location = new System.Drawing.Point(130, 190);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 15);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Press X to stop";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label7.Location = new System.Drawing.Point(10, 190);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(103, 15);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Press Z to start";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Green;
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.button5.Location = new System.Drawing.Point(290, 54);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(171, 23);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Start";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click_2);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(30, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 15);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Fruequncy (ms)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(187, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 21;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(320, 160);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(114, 15);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Test Auto Clicker";
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(340, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 23;
+            this.button1.Text = "Clicks";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(150, 140);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 15);
+            this.label9.TabIndex = 25;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "5",
+            "10",
+            "15",
+            "20",
+            "25",
+            "30",
+            "35",
+            "40",
+            "45",
+            "50",
+            "55",
+            "60",
+            "65",
+            "70",
+            "75",
+            "80",
+            "85",
+            "90",
+            "95",
+            "100"});
+            this.comboBox1.Location = new System.Drawing.Point(23, 110);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 26;
+            this.comboBox1.Tag = "";
+            this.comboBox1.Text = "Select Frequency";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(471, 212);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Mouse Clicker -UnderDog656";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ MouseClicker/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Hotkeys;
+
+namespace MouseClicker
+{
+    public partial class Form1 : Form
+    {
+        private Hotkeys.GlobalHotkey hk;
+        private Hotkeys.GlobalHotkey hk1;
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+        public string Saved = "";
+        public int cord;
+        private int cord1;
+        public int test123;
+        private int test1234;
+        public int act_position1;
+        public int act_position2;
+        private int clicks = 0;
+        private BackgroundWorker _worker = null;
+        private BackgroundWorker _worker1;
+        bool edited;
+        public int pos;
+        private bool check = true;
+        private bool check1 = true;
+        private bool check2 = true;
+        public DialogResult Msg_Box { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+            hk = new Hotkeys.GlobalHotkey(Constants.NOMOD, Keys.X, this);
+            hk1 = new Hotkeys.GlobalHotkey(Constants.NOMOD, Keys.Z, this);
+        }
+        private Keys GetKey(IntPtr LParam)
+        {
+            return (Keys)((LParam.ToInt32()) >> 16);
+        }
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == Constants.WM_HOTKEY_MSG_ID)
+            {
+                switch (GetKey(m.LParam))
+                {
+                    case Keys.Z:
+                        if (textBox3.Text.Length == 0 || comboBox1.Text == "" || comboBox1.Text == "Select Frequency" || comboBox1.Text == "0")
+                        {
+                            if (check == true)
+                            {
+                                check = false;
+                                button4.PerformClick();
+                            }
+                        }
+                        else if (textBox3.Text.StartsWith("{") && Convert.ToInt32(comboBox1.Text) >= 1 && Convert.ToInt32(comboBox1.Text) <= 100000)
+                        {
+                            if (check1 == true)
+                            {
+                                if (_worker1 == null)
+                                {
+                                        button4.PerformClick();
+                                        check1 = false;
+                                }
+                                else
+                                {
+                                    if (check2 == true)
+                                    {
+                                        if (_worker1.IsBusy)
+                                        {
+                                            check2 = false;
+                                        }
+                                        else
+                                        {
+                                            _worker1.CancelAsync();
+                                            check2 = false;
+                                            button4.PerformClick();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        break;
+                    case Keys.X:
+                        check1 = true;
+                        check2 = true;
+                        if (_worker1 == null)
+                        {
+
+                        }
+                        else
+                        {
+                            _worker1.CancelAsync();
+                        }
+                        break;
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.ReadOnly = true;
+            label2.ForeColor = Color.Red;
+            if (hk.Register())
+            {
+            }
+            if (hk1.Register())
+            {
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string num = e.KeyChar.ToString();
+            if (num == "a" || num == "A")
+            {
+                if (label9.Text == "")
+                {
+                    label9.Text = "Current Coordinates";
+                }
+                textBox3.Text = "{X=" + test123.ToString() + "," + "Y=" + test1234.ToString() + "}";
+                test123 = cord;
+                test1234 = cord1;
+                Saved = cord.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            _worker.CancelAsync();
+        }
+        private void UpdateTextbox(string _Text)
+
+        {
+            textBox1.Text = _Text;
+            textBox1.Text = "{X=" + cord.ToString() + "," + "Y=" + cord1.ToString() + "}";
+        }
+        
+
+        private void stop(object sender, EventArgs e)
+        {
+            _worker.CancelAsync();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "" || comboBox1.Text == "Select Frequency" || comboBox1.Text == "0" || test123.Equals(0)||test123.Equals(0))
+            {
+                Msg_Box = MessageBox.Show(" Make sure that you have chosen a frequency and correct cordinates ", "Error",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if (Msg_Box == DialogResult.OK)
+                {
+                    check = true;
+                }
+            }
+            else
+            {
+                pos = Convert.ToInt32(comboBox1.Text);
+                _worker1 = new BackgroundWorker();
+                _worker1.WorkerSupportsCancellation = true;
+
+                _worker1.DoWork += new DoWorkEventHandler((state, args) =>
+                {
+                    do
+                    {
+                        if (_worker1.CancellationPending)
+                            break;
+                        else
+                        {
+                            Control control = new Control();
+                            System.Windows.Forms.Cursor.Position = control.PointToScreen(new Point(test123-8, test1234-31));
+                            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+                            System.Threading.Thread.Sleep(pos);
+                        }
+                    } while (true);
+                });
+                _worker1.RunWorkerAsync();
+            }
+        }
+        private void button5_Click_2(object sender, EventArgs e)
+        {
+            label2.Text = "Press A to save cordinates";
+            Button button = sender as Button;
+            if (button == null) return;
+
+            if (button.Text != "Start")
+            {
+                label2.Text = "";
+                label5.Text = "";
+                button.Text = "Start";
+                button.BackColor = Color.Green;
+                if (_worker == null)
+                {
+
+                }
+                else
+                    _worker.CancelAsync();
+            }
+            else if (button.Text == "Start")
+            {
+                label5.Text = "Current Position";
+                button.Text = "Stop";
+                button.BackColor = Color.Red;
+                _worker = new BackgroundWorker();
+                _worker.WorkerSupportsCancellation = true;
+
+                _worker.DoWork += new DoWorkEventHandler((state, args) =>
+                {
+                    do
+                    {
+                        if (_worker.CancellationPending)
+                            break;
+                        else
+                        {
+                            cord = MousePosition.X;
+                            cord1 = MousePosition.Y;
+                            System.Threading.Thread.Sleep(1);
+                            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(Form1_KeyPress);
+                            this.Invoke(new Action<string>(UpdateTextbox), new object[] { "123" });
+                        }
+                    } while (true);
+                });
+                _worker.RunWorkerAsync();
+            }
+        }
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            edited = !char.IsControl(e.KeyChar);
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clicks++;
+            button1.Text = Convert.ToString(clicks);
+        }
+    }
+}
